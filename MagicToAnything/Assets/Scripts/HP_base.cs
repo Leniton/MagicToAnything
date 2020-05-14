96cc5cd19141c0e08847e35f1ea455e253cdee80
Leniton <48127860+Leniton@users.noreply.github.com>
2020-05-14 13:48:53 -0300

    HP

---
+++ MagicToAnything/Assets/Scripts/HP_base.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HP_base : MonoBehaviour
+{
+    public int MAX_HP;
+    public int HP;
+
+    [SerializeField] bool Invicible;
+    [SerializeField] bool BlockHeal;
+ 
+    public void Damage(int dmg)
+    {
+        if (Invicible) return;
+
+        HP -= dmg;
+    }
+
+    public void Heal(int hl)
+    {
+        if (BlockHeal) return;
+
+        HP += hl;
+        if(HP > MAX_HP)
+        {
+            HP = MAX_HP;
+        }
+    }
+}
